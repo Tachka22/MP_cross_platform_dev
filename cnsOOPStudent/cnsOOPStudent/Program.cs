447c1bc7819c144e5a42f3f17ad3a0ac39dd290b
Vlad <chinnov.vlad@gmail.com>
2023-10-10 13:50:41 +0300

    oop

---
+++ cnsOOPStudent/cnsOOPStudent/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace cnsOOPStudent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.AgeChange += Student_AgeChange;
+            student.AgeChange2 += Student_AgeChange2;
+            student.Name = "Иван";
+            student.Surname = "Иванович";
+            student.Age = 10;
+            Console.WriteLine(student.GetFullName());
+            student.Age = 22;
+            Console.WriteLine(student.GetFullName());
+           
+        }
+
+        private static void Student_AgeChange2(object? sender, int age)
+        {
+            Console.WriteLine($"Новый возраст, {age}");
+        }
+
+        private static void Student_AgeChange(object? sender, EventArgs e)
+        {
+            if(sender is Student v)
+                Console.WriteLine($"Изменился возраст, {v.Age}");
+
+        }
+    }
+}
---
+++ cnsOOPStudent/cnsOOPStudent/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cnsOOPStudent
+{
+    internal class Student
+    {
+        private int age;
+
+        public event EventHandler AgeChange;
+
+        public delegate void EventHandlerAge(object? sender, int age);
+        public event EventHandlerAge AgeChange2;
+        public string Name { get; internal set; }
+        public string Surname { get; internal set; }
+        public int Age
+        {
+            get 
+            { 
+                return age;
+            }
+            internal set
+            {
+                if (value > 0)
+                    age = value;
+                AgeChange?.Invoke(this,  EventArgs.Empty);
+                AgeChange2?.Invoke(this, Age);
+                //if(AgeChange  != null)
+                //    AgeChange.Invoke(this, new EventArgs());
+            }
+        }
+       
+        public string GetFullName()
+        {
+            return $"{Name} {Surname} ({Age})";
+        }
+
+    }
+}
