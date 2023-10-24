0d0088a0bd6e78de17cc6cc9c15f4c3457513d2b
Vlad <chinnov.vlad@gmail.com>
2023-10-24 14:19:34 +0300

    Реализовал класс кейса нашего примера, и добавление его в стек в случае пропуска

---
+++ wfGameTrainerAccount/wfGameTrainerAccount/Case.cs
@@ -0,0 +1,25 @@
+﻿
+namespace wfGameTrainerAccount
+{
+    public class Case
+    {
+        public int xValue1 {  get;  set; }
+        public int xValue2 { get; set;}
+        public double xResult { get; set;}
+        public char Symbol { get; set; } 
+        public double xResultNew { get; set; }
+
+        public Case()
+        {
+        }
+
+        public Case(int xValue1, int xValue2, double xResult, char symbol, double xResultNew)
+        {
+            this.xValue1 = xValue1;
+            this.xValue2 = xValue2;
+            this.xResult = xResult;
+            Symbol = symbol;
+            this.xResultNew = xResultNew;
+        }
+    }
+}
