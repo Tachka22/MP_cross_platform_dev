ad3897f32896ec32c02c2a0897ad94368d23f649
Vlad <chinnov.vlad@gmail.com>
2023-11-07 13:52:37 +0300

    Маршрутизация

---
+++ wpfEventRoute/wpfEventRoute/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpfEventRoute
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClear_Click(object sender, RoutedEventArgs e)
+        {
+            lbName.Items.Clear();
+        }
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            lbName.Items.Add($"TextBlock_MouseDown \n sender = {sender} \n {e.Source}");
+        }
+    }
+}
