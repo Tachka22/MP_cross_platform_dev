8ebb45470d4deb853c16eac4478871d197f5b17e
Vlad <chinnov.vlad@gmail.com>
2024-01-15 10:43:35 +0300

    карта(доработать)

---
+++ cnsHomework10.10.23/cnsGenMapMaze/Game.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cnsGenMapMaze
+{
+    internal class Game
+    {
+        private int height;
+        private int width;
+        private int[] start;
+        private int[] end;
+        private char[][] maze;
+
+        public Game(int height, int width, int[] start = null, int[] end = null)
+        {
+            this.height = height;
+            this.width = width;
+            this.start = start != null ? start : new int[] { 0, new Random().Next(1, width - 2) };
+            this.end = end != null ? end : new int[] { height - 1, new Random().Next(1, width - 2) };
+        }
+
+        public void PrintMaze()
+        {
+
+        }
+    }
+}
---
+++ cnsHomework10.10.23/cnsGenMapMaze/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace cnsGenMapMaze
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var g = new Game(7, 9);
+            g.PrintMaze();
+        }
+    }
+}
